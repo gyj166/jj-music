1c10c49471b5b23a294d73d5e2e09fe611c82f12
gyj166 <131328899+gyj166@users.noreply.github.com>
2023-04-20 11:52:13 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Asleep)
+basic.showIcon(IconNames.Tortoise)
+basic.showIcon(IconNames.Fabulous)
+basic.showIcon(IconNames.No)
+basic.forever(function () {
+    music.playMelody("A D F C G F F B ", 120)
+})
